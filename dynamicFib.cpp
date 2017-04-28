9f6dbfdeabda5e3ba02c138626edf88c8d565b1e
Kerem Hallaç <hallackerem@gmail.com>
2017-04-28 13:31:05 +0300

    Update dynamicFib.cpp

--- dynamicFib.cpp
+++ dynamicFib.cpp
@@ -6,22 +6,26 @@ using namespace std;
 int fib(int n)
 {
  
- if(n<=1)
- {
-   return 1;              
- }
- else
- {
-     int f = fib(n-1) +fib(n-2);
-     return f;
- }
+  int f[n+1];
+  int i;
+  f[0] = 0;
+  f[1] = 1;
+  for( i = 2; i<= n; i++)
+  {
+     f[i] = f[i-1] + f[i-2];
+  }
+  
+  return f[n];
     
 }
 
 int main(int argc, char *argv[])
 {
-    int sonuc = fib(10);
-    cout << sonuc;
+    int n = 10;
+    cout << fib(n);
+
+    matriscarpimi();
+    
     system("PAUSE");
     return EXIT_SUCCESS;
 }
